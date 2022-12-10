07c3a061692c0387cb98733f3056ea51e7aaf9d6
vsc-sxx <shelbyhell@proton.me>
2022-12-10 05:30:02 +0000

    juice: Build libwpa_client.
    Fix:
    12-10 09:19:26.427  1193  1193 F linker  : CANNOT LINK EXECUTABLE "lowi-server": library "libwpa_client.so" not found: needed by main executable
    12-10 09:19:26.502  1246  1246 F linker  : CANNOT LINK EXECUTABLE "lowi-server": library "libwpa_client.so" not found: needed by main executable
    12-10 09:19:26.537  1315  1315 F linker  : CANNOT LINK EXECUTABLE "lowi-server": library "libwpa_client.so" not found: needed by main executable
    12-10 09:19:26.569  1336  1336 F linker  : CANNOT LINK EXECUTABLE "lowi-server": library "libwpa_client.so" not found: needed by main executable
    12-10 09:19:26.597  1342  1342 F linker  : CANNOT LINK EXECUTABLE "lowi-server": library "libwpa_client.so" not found: needed by main executable
    12-10 09:19:26.642  1346  1346 F linker  : CANNOT LINK EXECUTABLE "lowi-server": library "libwpa_client.so" not found: needed by main executable
    12-10 09:19:26.695  1275  1275 F linker  : CANNOT LINK EXECUTABLE "/vendor/bin/imsdatadaemon": library "libwpa_client.so" not found: needed by main executable
    12-10 09:19:26.701  1138  1138 F linker  : CANNOT LINK EXECUTABLE "/system/vendor/bin/cnd": library "libwpa_client.so" not found: needed by /vendor/lib64/libcne.so in namespace (default)
    12-10 09:19:30.904  1936  1936 F linker  : CANNOT LINK EXECUTABLE "/system/vendor/bin/cnd": library "libwpa_client.so" not found: needed by /vendor/lib64/libcne.so in namespace (default)
    12-10 09:19:31.485  1958  1958 F linker  : CANNOT LINK EXECUTABLE "/vendor/bin/imsdatadaemon": library "libwpa_client.so" not found: needed by main executable
    12-10 09:19:35.904  2021  2021 F linker  : CANNOT LINK EXECUTABLE "/system/vendor/bin/cnd": library "libwpa_client.so" not found: needed by /vendor/lib64/libcne.so in namespace (default)
    12-10 09:19:36.488  2023  2023 F linker  : CANNOT LINK EXECUTABLE "/vendor/bin/imsdatadaemon": library "libwpa_client.so" not found: needed by main executable
    12-10 09:19:40.917  2634  2634 F linker  : CANNOT LINK EXECUTABLE "/system/vendor/bin/cnd": library "libwpa_client.so" not found: needed by /vendor/lib64/libcne.so in namespace (default)
    12-10 09:19:41.492  2672  2672 F linker  : CANNOT LINK EXECUTABLE "/vendor/bin/imsdatadaemon": library "libwpa_client.so" not found: needed by main executable
    
    Signed-off-by: vsc-sxx <shelbyhell@proton.me>

--- device.mk
+++ device.mk
@@ -502,6 +502,7 @@ PRODUCT_PACKAGES += \
     hostapd \
     libwifi-hal-ctrl \
     libwifi-hal-qcom \
+    libwpa_client \
     vendor.qti.hardware.wifi.hostapd@1.2.vendor \
     vendor.qti.hardware.wifi.supplicant@2.2.vendor \
     wpa_supplicant \

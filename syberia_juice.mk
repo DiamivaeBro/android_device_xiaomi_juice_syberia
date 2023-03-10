d2805b77971940633628d546d1adce22e7753d23
DiamivaeBro <119186976+DiamivaeBro@users.noreply.github.com>
2023-03-10 09:33:27 +0300

    Adapted for SyberiaOs
    From CherishOs to SyberiaOs

--- syberia_juice.mk
+++ syberia_juice.mk
@@ -2,8 +2,8 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common CherishOS stuff.
-$(call inherit-product, vendor/cherish/config/common_full_phone.mk)
+# Inherit some common SyberiaOS stuff.
+$(call inherit-product, vendor/syberia/common.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_SUPPORTS_QUICK_TAP := true
 
@@ -11,7 +11,7 @@ TARGET_SUPPORTS_QUICK_TAP := true
 $(call inherit-product, device/xiaomi/juice/device.mk)
 
 # Device identifier
-PRODUCT_NAME := cherish_juice
+PRODUCT_NAME := syberia_juice
 PRODUCT_DEVICE := juice
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := SM6115
@@ -19,8 +19,7 @@ PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
-# CherishOS Stuff with GApps
-TARGET_SUPPORTS_GOOGLE_RECORDER := true
-TARGET_FACE_UNLOCK_SUPPORTED := true
-TARGET_ENABLE_BLUR := true
-WITH_GMS := true
+
+
+
+

171c69817f8e90c93fc23a86f27ae411c8321f84
DiamivaeBro <119186976+DiamivaeBro@users.noreply.github.com>
2023-03-10 09:35:50 +0300

    Adapted for SyberiaOs
    From CherishOs to SyberiaOs

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,7 +1,7 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/cherish_juice.mk
+    $(LOCAL_DIR)/syberia_juice.mk
 
 COMMON_LUNCH_CHOICES := \
-    cherish_juice-user \
-    cherish_juice-userdebug \
-    cherish_juice-eng
+    syberia_juice-user \
+    syberia_juice-userdebug \
+    syberia_juice-eng
